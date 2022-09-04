78a7a9b0a8437d1128b841778387af2426f15f7f
unknown <tamiloreolaniyi@gmail.com>
2022-09-04 16:11:47 +0100

    setting up

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config();
+
+mongoose.connect(
+        process.env.MONGO_URL
+    )
+    .then(() => console.log('DB Connection Succesful!'))
+    .catch((err) => {
+        console.log(err);
+    })
+
+app.get("/api/test", () => {
+    console.log('test is successful')
+});
+
+app.listen(5000, () => {
+    console.log('back end server is running');
+});
