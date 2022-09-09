cf591c437955cb87f104405df400e56afdb262cc
unknown <tamiloreolaniyi@gmail.com>
2022-09-09 14:15:59 +0100

    login

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 const User = require('../models/User')
+const CryptoJS = require("crypto-js")
 
 //register
 router.post('/register', async(req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
     });
 
     try {
@@ -17,6 +18,30 @@ router.post('/register', async(req, res) => {
         console.log(err);
         res.status(500).json(err);
     }
+})
+
+//login
+router.post('/login', async(req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        !user && res.status(401).json("Wrong credentials")
+
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.PASS_SEC
+        );
+        const Originalpassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+        Originalpassword !== req.body.password &&
+            res.status(401).json("Wrong credentials");
+
+        const { password, ...others } = user._doc;
+
+        res.status(200).json(user);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+
 })
 
 module.exports = router;
