67c5a92b49bbd39fd30c72bf338e04929a102c5a
unknown <tamiloreolaniyi@gmail.com>
2022-09-24 16:40:27 +0100

    update username

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,32 @@
 const router = require('express').Router();
 const { verifyToken, verifyTokenAndAuthorization } = require('./verifyToken')
+const User = require("../models/User");
 
-router.put("/:id", verifyTokenAndAuthorization, (req, res) => {
+
+//Update
+router.put("/:id", verifyTokenAndAuthorization, async(req, res) => {
     if (req.body.password) {
-
+        req.body.password = CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SEC
+        ).toString();
     }
+
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id, {
+                $set: req.body,
+            }, { new: true }
+        );
+        res.status(200).json(updatedUser);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//delete
+router.delete("/:id", verifyTokenAndAuthorization, async(req, res) => {
+
 })
 
 
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const verifyToken = (req, res, next) => {
     const authHeader = req.headers.token;
     if (authHeader) {
+        const token = authHeader.split(" ")[1];
         jwt.verify(token, process.env.JWT_SEC, (err, user) => {
             if (err) res.status(403).json("Token is not valid");
             req.user = user;
@@ -23,4 +24,14 @@ const verifyTokenAndAuthorization = (req, res, next) => {
     })
 }
 
-module.exports = { verifyToken, verifyTokenAndAuthorization }
+const verifyTokenAndAdmin = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if (req.user.isAdmin) {
+            next()
+        } else {
+            res.status(403).json("You do not have permission!")
+        }
+    })
+}
+
+module.exports = { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin }
