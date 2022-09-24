b211bb7c31dbfc28f151c226f0b7ebe0756cbcc3
unknown <tamiloreolaniyi@gmail.com>
2022-09-24 10:27:00 +0100

    verify auth

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
+const { verifyToken, verifyTokenAndAuthorization } = require('./verifyToken')
 
-router.put("/:id", )
+router.put("/:id", verifyTokenAndAuthorization, (req, res) => {
+    if (req.body.password) {
+
+    }
+})
 
 
 module.exports = router;
