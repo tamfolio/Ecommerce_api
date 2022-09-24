b684ec9700de70991d42b9df0c4b4319638e5886
unknown <tamiloreolaniyi@gmail.com>
2022-09-24 16:43:11 +0100

    delete method

--- routes/user.js
+++ routes/user.js
@@ -26,7 +26,12 @@ router.put("/:id", verifyTokenAndAuthorization, async(req, res) => {
 
 //delete
 router.delete("/:id", verifyTokenAndAuthorization, async(req, res) => {
-
+    try {
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200).json("User has been Deleted...")
+    } catch (err) {
+        res.status(500).json(err);
+    }
 })
 
 
