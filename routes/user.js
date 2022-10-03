44ced3a3aaca503af6b94a5a2aee3ebaf57fb9d4
unknown <tamiloreolaniyi@gmail.com>
2022-10-03 15:54:39 +0100

    api update

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { verifyToken, verifyTokenAndAuthorization } = require('./verifyToken')
+const { verifyTokenAndAuthorization, verifyTokenAndAdmin } = require('./verifyToken')
 const User = require("../models/User");
 
 
@@ -29,6 +29,29 @@ router.delete("/:id", verifyTokenAndAuthorization, async(req, res) => {
     try {
         await User.findByIdAndDelete(req.params.id)
         res.status(200).json("User has been Deleted...")
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+//Get User
+router.get("/find/:id", verifyTokenAndAdmin, async(req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        const { password, ...others } = user._doc;
+
+        res.status(200).json(others);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+//Get all Users
+router.get("/", verifyTokenAndAdmin, async(req, res) => {
+    const query = req.query.new;
+    try {
+        const users = query ? await User.find().sort({ _id: -1 }).limit(1) : await User.find();
+        res.status(200).json(users);
     } catch (err) {
         res.status(500).json(err);
     }
