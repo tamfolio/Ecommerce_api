42bd394160856e525101c03d26115502cf02c4a8
unknown <tamiloreolaniyi@gmail.com>
2022-10-03 18:39:23 +0100

    update cart

--- routes/cart.js
+++ routes/cart.js
@@ -14,19 +14,19 @@ router.post('/', verifyToken, async(req, res) => {
     }
 })
 
-// //Update
-// router.put("/:id", verifyTokenAndAdmin, async(req, res) => {
-//     try {
-//         const updatedProduct = await Product.findByIdAndUpdate(
-//             req.params.id, {
-//                 $set: req.body,
-//             }, { new: true }
-//         );
-//         res.status(200).json(updatedProduct);
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
+//Update
+router.put("/:id", verifyTokenAndAuthorization, async(req, res) => {
+    try {
+        const updatedCart = await Cart.findByIdAndUpdate(
+            req.params.id, {
+                $set: req.body,
+            }, { new: true }
+        );
+        res.status(200).json(updatedCart);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 // //delete
 // router.delete("/:id", verifyTokenAndAdmin, async(req, res) => {
