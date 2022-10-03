6b883018a2d8b44e00802e66f4b592176074facc
unknown <tamiloreolaniyi@gmail.com>
2022-10-03 17:07:41 +0100

    add product

--- models/Product.js
+++ models/Product.js
@@ -31,4 +31,4 @@ const ProductSchema = new mongoose.Schema({
     },
 }, { timestamps: true });
 
-module.exports = mongoose.model("User", ProductSchema);
+module.exports = mongoose.model("Product", ProductSchema);
